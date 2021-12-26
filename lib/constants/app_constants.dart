9911abf568f13fd1fe4acf0c235c3ad91739cfd6
MrRoy121 <nilashishroyjoy@gmail.com>
2021-12-26 21:22:16 +0600

    Bmi

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#646464");
+final Color accentHexColor = HexColor("#00ffff");
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bmi_calculator/screen/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
